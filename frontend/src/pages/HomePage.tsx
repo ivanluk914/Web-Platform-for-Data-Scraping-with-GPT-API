fe9708e6c338f53343ae9fcff5c9cc1f1994c654
z3378533 <z3378533@ad.unsw.edu.au>
2024-10-09 22:36:12 +1100

    added toaster welcome message to landing on home page for first time

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
 import Sidebar from '../components/SideBar';
 import Content from '../components/Contents';
 
 const HomePage = () => {
+  useEffect(() => {
+    const hasVisited = localStorage.getItem('hasVisitedHomePage');
+    
+    if (!hasVisited) {
+      toast.success('Welcome, Jane Doe!');
+      localStorage.setItem('hasVisitedHomePage', 'true');
+    }
+  }, []);
+
   return (
     <div className="flex h-full bg-white">
       <Sidebar />
       <Content />
+      <Toaster position="top-center" reverseOrder={false} toastOptions={{duration: 5000}}/>
     </div>
   );
 };
