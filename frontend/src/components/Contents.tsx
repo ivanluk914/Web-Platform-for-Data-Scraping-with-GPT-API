f7767dd3911e9e1b36e8da39384fbc3655c7024a
Junle Zhao <z5447039@ad.unsw.edu.au>
2024-10-11 18:43:39 +0800

    I use nextui to replace old ui, and add button function, remove change page stuff

--- frontend/src/components/Contents.tsx
+++ frontend/src/components/Contents.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import ProfilePage from '../pages/ProfilePage';
 import AdminPage from '../pages/AdminPage';
+import Createtask from '../pages/CreateTask';
+
 
 const Content = () => {
   return (
@@ -8,7 +10,7 @@ const Content = () => {
       <div className="p-4">
         <Routes>
           <Route path="/" element={<h1>Home Content</h1>} />
-          <Route path="task-creation" element={<h1>Task Creation</h1>} />
+          <Route path="task-creation" element={<Createtask/>} />
           {/* Task Management Sub-pages */}
           <Route path="task-management/ongoing" element={<h1>On Going Tasks</h1>} />
           <Route path="task-management/completed" element={<h1>Completed Tasks</h1>} />
