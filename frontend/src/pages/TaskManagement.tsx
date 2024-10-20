b0b7652d679e7b1b9e68b279d5079c14c4360025
Ivan Luk <ivanluk@MBA-2.local>
2024-10-20 09:53:08 +0800

    Updated Based on Review + Fixed HomePage SideBar Bug

--- frontend/src/pages/TaskManagement.tsx
+++ frontend/src/pages/TaskManagement.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip, Tooltip, Link } from "@nextui-org/react";
 import { useNavigate } from 'react-router-dom';
-import { EyeIcon } from "../components/EyeIcon";
-import { DeleteIcon } from "../components/DeleteIcon";
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
+import { MdOutlineDeleteForever } from "react-icons/md";
+import { GrView } from "react-icons/gr";
 
 interface Task {
   id: string;
@@ -112,14 +112,14 @@ const TaskManagement: React.FC = () => {
       case "actions":
         return (
           <div className="relative flex items-center justify-center gap-2">
-            <Tooltip content="View Details">
+            <Tooltip content="View Details" delay={0} closeDelay={0} size="sm">
               <span className="text-lg text-default-400 cursor-pointer active:opacity-50" onClick={() => handleViewDetails(task.id)}>
-                <EyeIcon />
+                <GrView />
               </span>
             </Tooltip>
-            <Tooltip color="danger" content="Delete Task">
+            <Tooltip color="danger" content="Delete Task" delay={0} closeDelay={0} size="sm">
               <span className="text-lg text-danger cursor-pointer active:opacity-50" onClick={() => handleDeleteTask(task.id)}>
-                <DeleteIcon />
+                <MdOutlineDeleteForever />  
               </span>
             </Tooltip>
           </div>
