7342db67a78c1975e3c5d53e68c3fd59f4dad216
Ivan Luk <ivanluk@MBA-2.local>
2024-10-30 19:53:15 +0800

    Removed Auth0 hook

--- frontend/src/components/RouteProtector.tsx
+++ frontend/src/components/RouteProtector.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
 import { Skeleton } from '@nextui-org/react';
 import { useUser } from '../providers/user-provider';
 
@@ -10,23 +9,17 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, requiredRoles }) => {
-  const { isAuthenticated } = useAuth0();
   const { currentUser, isLoading } = useUser();
   const [hasAccess, setHasAccess] = useState<boolean | null>(null);
 
   useEffect(() => {
-    if (!isAuthenticated || isLoading) {
-      setHasAccess(false);
-      return;
-    }
-
     if (currentUser) {
       const hasRequiredRole = currentUser.roles?.some(role => requiredRoles.includes(role)) ?? false;
       setHasAccess(hasRequiredRole);
     } else {
       setHasAccess(false);
     }
-  }, [isAuthenticated, isLoading, currentUser, requiredRoles]);
+  }, [ isLoading, currentUser, requiredRoles]);
 
   if (hasAccess === null || isLoading) {
     return (
