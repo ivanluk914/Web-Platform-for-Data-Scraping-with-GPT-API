1a1227123f48b6d043185ee388aa9e2aac76e3d3
Ivan Luk <ivanluk@MBA-2.local>
2024-10-31 13:00:10 +0800

    Improved hasAccess logic + Added error message to unauthorized users

--- frontend/src/components/RouteProtector.tsx
+++ frontend/src/components/RouteProtector.tsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { Skeleton } from '@nextui-org/react';
 import { useUser } from '../providers/user-provider';
 import { hasRole } from '../models/user';
+import { toast } from 'react-hot-toast';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -12,15 +13,25 @@ interface ProtectedRouteProps {
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, requiredRoles }) => {
   const { currentUser, isLoading } = useUser();
   const [hasAccess, setHasAccess] = useState<boolean | null>(null);
+  const [toastShown, setToastShown] = useState(false); 
 
   useEffect(() => {
-    if (currentUser) {
+    if (!isLoading && currentUser) {
       const hasRequiredRole = requiredRoles.some(role => hasRole(currentUser, role));
       setHasAccess(hasRequiredRole);
-    } else {
+      setToastShown(false);
+
+    } else if (!isLoading) {
       setHasAccess(false);
     }
-  }, [ isLoading, currentUser, requiredRoles]);
+  }, [isLoading, currentUser, requiredRoles]);
+
+  useEffect(() => {
+    if (hasAccess === false && !toastShown) {
+      toast.error('You do not have access to this page');
+      setToastShown(true);
+    }
+  }, [hasAccess, toastShown]);
 
   if (hasAccess === null || isLoading) {
     return (
